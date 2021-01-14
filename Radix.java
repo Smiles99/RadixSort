c3a0bd71506ca38ff50cff6f4df5c274401c07b0
Smiles99 <mdavies10@stuy.edu>
2021-01-14 09:49:16 -0500

    Update Radix.java

--- Radix.java
+++ Radix.java
@@ -63,6 +63,38 @@ public class Radix {
         data.extend(output);
     }
     public static void radixSort(SortableLinkedList data) {
-
+        final int RADIX = 10;
+        SortableLinkedList[] bucket = new SortableLinkedList[RADIX];
+        SortableLinkedList output = new SortableLinkedList();
+        int maxValue = -1;
+        for (int i =0; i<data.size(); i++) {
+            if (data.get(i) > maxValue) {
+                maxValue = data.get(i);
+            }
+        }
+        for (int i = 0; i < bucket.length; i++)  {
+            bucket[i] = new SortableLinkedList();
+        }
+        int place = 1;
+        output.extend(data);
+        while (maxValue > place) {
+            System.out.println("before place: " + place);
+            System.out.println(output);
+            int digit;
+            for (int i = 0; i<output.size(); i++) {
+                int value = output.get(i);
+                digit = value / place;
+                bucket[digit % RADIX].add(value);
+            }
+            System.out.println("after place: " + place);
+            output = new SortableLinkedList();
+            for (int b = 0; b < RADIX; b++) {
+                System.out.println("bucket " + b + ": " + bucket[b]);
+                output.extend(bucket[b]);
+            }
+            System.out.println(output);
+            place *= RADIX;
+        }
+        data.extend(output);
     }
 }
